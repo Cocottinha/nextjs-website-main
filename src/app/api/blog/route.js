2f31f2255893f39115df4ef48c62dbe1d5075ca1
Lucas Cotta Muniz <lucascottamuniz@hotmail.com>
2024-06-05 11:49:15 -0300

    agora sem mongoDB e sem mongoose

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,12 +1,8 @@
-import { connectToDB } from "@/lib/connectToDB"
-import { Post } from "@/lib/models"
+
 import { NextResponse } from "next/server"
 
 export const GET = async (request) =>{
     try {
-        connectToDB()
-        const posts = await Post.find()
-        return NextResponse.json(posts)
 
     } catch (error) {
         console.log(error)
